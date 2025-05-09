c95561981d5b7090714f22c56c02cc884c60b9c3
Hans <hsvchcl@gmail.com>
2025-05-09 11:22:06 -0400

    add enviroment file...

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  geoApiUrl: 'https://ipinfo.io/json',
+  geoApiKey: '',
+  openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  openWeatherForecastUrl: 'https://api.openweathermap.org/data/2.5/forecast',
+  openWeatherApiKey: '',
+};
