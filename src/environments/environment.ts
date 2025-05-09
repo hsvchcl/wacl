e00d31ddbe2539b4b9b2702e77aa77759b4bd8ea
Hans <hsvchcl@gmail.com>
2025-05-09 10:28:50 -0400

    remove enviroment vars..

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  geoApiUrl: 'https://ipinfo.io/json',
-  geoApiKey: 'a7765afdb518f4',
-  openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5/weather',
-  openWeatherForecastUrl: 'https://api.openweathermap.org/data/2.5/forecast',
-  openWeatherApiKey: '9253c59e938ab64d8f625eafa1fff6be',
-};
